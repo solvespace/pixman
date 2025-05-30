aafa6b51f608a46a33ad23dfe13c3c7bd8ab10a1
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-05-30 12:01:54 +0300

    region: add translatef function for fractional regions
    Signed-off-by: Loukas Agorgianitis <loukas@agorgianitis.com>

--- pixman/pixman.h
+++ pixman/pixman.h
@@ -827,6 +827,11 @@ void                    pixman_region64f_translate         (pixman_region64f_t *
 							    int                 y);
 
 PIXMAN_API
+void                    pixman_region64f_translatef        (pixman_region64f_t *region,
+							    double              x,
+							    double              y);
+
+PIXMAN_API
 pixman_bool_t           pixman_region64f_copy              (pixman_region64f_t       *dest,
 							    const pixman_region64f_t *source);
 
